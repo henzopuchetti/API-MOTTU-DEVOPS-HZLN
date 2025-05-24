a0f86a1f08486795d5bc22dec55e4cf1f30d969d
henzo <84385799+henzopuchetti@users.noreply.github.com>
2025-05-23 21:59:59 -0300

    Add files via upload

---
+++ src/main/java/com/fiap/mottu_patio/config/Insert.java
@@ -0,0 +1,24 @@
+package com.fiap.mottu_patio.config;
+
+import com.fiap.mottu_patio.model.Patio;
+import com.fiap.mottu_patio.repository.PatioRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Insert {
+
+    @Bean
+    CommandLineRunner initDatabase(PatioRepository patioRepository) {
+        return args -> {
+            if (patioRepository.count() == 0) {
+                patioRepository.save(new Patio(null, "Pátio Zona Norte", "Rua das Palmeiras, 123", 50));
+                patioRepository.save(new Patio(null, "Pátio Central", "Avenida Brasil, 987", 75));
+                patioRepository.save(new Patio(null, "Pátio Sul", "Rua do Comércio, 456", 100));
+                patioRepository.save(new Patio(null, "Pátio Leste", "Alameda Santos, 234", 80));
+                patioRepository.save(new Patio(null, "Pátio Oeste", "Travessa das Rosas, 789", 60));
+            }
+        };
+    }
+}
