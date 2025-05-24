2dd1725ecf421a6823e7b63b588879f401256c12
henzo <84385799+henzopuchetti@users.noreply.github.com>
2025-05-23 22:14:54 -0300

    Add files via upload

--- src/main/java/com/fiap/mottu_patio/config/Insert.java
+++ src/main/java/com/fiap/mottu_patio/config/Insert.java
@@ -13,11 +13,13 @@ public class Insert {
     CommandLineRunner initDatabase(PatioRepository patioRepository) {
         return args -> {
             if (patioRepository.count() == 0) {
-                patioRepository.save(new Patio(null, "Pátio Zona Norte", "Rua das Palmeiras, 123", 50));
-                patioRepository.save(new Patio(null, "Pátio Central", "Avenida Brasil, 987", 75));
-                patioRepository.save(new Patio(null, "Pátio Sul", "Rua do Comércio, 456", 100));
-                patioRepository.save(new Patio(null, "Pátio Leste", "Alameda Santos, 234", 80));
-                patioRepository.save(new Patio(null, "Pátio Oeste", "Travessa das Rosas, 789", 60));
+                patioRepository.save(new Patio(null, "Pátio Zona Norte", "Rua das Palmeiras, 123", 50, 50));
+                patioRepository.save(new Patio(null, "Pátio Central", "Avenida Brasil, 987", 75, 75));
+                patioRepository.save(new Patio(null, "Pátio Sul", "Rua do Comércio, 456", 100, 100));
+                patioRepository.save(new Patio(null, "Pátio Leste", "Alameda Santos, 234", 80, 80));
+                patioRepository.save(new Patio(null, "Pátio Oeste", "Travessa das Rosas, 789", 60, 60));
+
+                System.out.println("✅ 5 pátios inseridos com sucesso no banco de dados!");
             }
         };
     }
